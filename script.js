443e9957aaa126617c5b616e00d352565d57269d
Becky Marshall <becky.marshall@id.iit.edu>
2017-04-16 16:15:16 -0500

    Been editing based on Jenni’s work. “Syntax Error: Unexpected token > “ (line 35) is driving me crazy. Is it seeing HTML or something?

--- script.js
+++ script.js
@@ -19,59 +19,48 @@
 //     - Otherwise, change the background color of the box to **'blue'**.
 
 var boxes = document.querySelectorAll('.box');
-
-
-if (boxes.length < 301 && > 200) {
-    console.log("less then 301, more than 200")
-    boxes[].style.backgroundColor = "yellow";
-} 
-
-else if (boxes.length < 401 && > 300) {
-    console.log("less then 401, more than 300")
-    boxes[].style.backgroundColor = "green";
-} 
-
-else if (boxes.length < 501 && > 400) {
-    console.log("less then 501, more than 400")
-    boxes[].style.backgroundColor = "purple";
-} 
-
-else if (boxes.length < 601 && > 500) {
-    console.log("less then 601, more than 500")
-    boxes[].style.backgroundColor = "orange";
-} 
-
-else if (boxes.length < 701 && > 600) {
-    console.log("less then 701, more than 600")
-    boxes[].style.backgroundColor = "pink";
-} 
-
-else if (boxes.length < 801 && > 700) {
-    console.log("less then 701, more than 600")
-    boxes[].style.backgroundColor = "black";
-} 
-
-else if (boxes.length < 901 && > 800) {
-    console.log("less then 701, more than 600")
-    boxes[].style.backgroundColor = "brown";
-} 
-
-else {
-    console.log("default")
-    boxes[].style.backgroundColor = "blue";
-}
-
-function boxClicked(){
-        console.log("Box clicked");
-}
-
+console.log("Total number of boxes: " + boxes.length)
 
 for (var i = 0; i < boxes.length; i++) {
-    console.log(i);
-    boxes[i].addEventListener("click", boxClicked);
-}
+    unclickedBoxes = boxes[i];
+    unclickedBoxes.addEventListener('click', function () {
+        if (this.className === "unclickedBoxes") {
+            this.className = "clickedBox";
+        } else {
+            this.className = "unclickedBoxes";
+        }
 
-for (var i = 0; i < 99; i++) {
-   console.log(i);
-   boxes[i].addEventListener("click", boxClicked);
-}
+        if (boxes.length <= 301 && boxes.length >= 201) {
+            console.log("less then 301, more than 200")
+            this.style.backgroundColor = "yellow";
+        }
+        else if (boxes.length < 401 && boxes.length > 300) {
+            console.log("less then 401, more than 300")
+            this.style.backgroundColor = "green";
+        }
+        else if (boxes.length < 501 && boxes.length > 400) {
+            console.log("less then 501, more than 400")
+            this.style.backgroundColor = "purple";
+        }
+        else if (boxes.length < 601 && boxes.length > 500) {
+            console.log("less then 601, more than 500")
+            this.style.backgroundColor = "orange";
+        }
+        else if (boxes.length < 701 && boxes.length > 600) {
+            console.log("less then 701, more than 600")
+            this.style.backgroundColor = "pink";
+        }
+        else if (boxes.length < 801 && boxes.length > 700) {
+            console.log("less then 701, more than 600")
+            this.style.backgroundColor = "black";
+        }
+        else if (boxes.length < 901 && boxes.length > 800) {
+            console.log("less then 701, more than 600")
+            this.style.backgroundColor = "brown";
+        }
+        else {
+            console.log("default")
+            this.style.backgroundColor = "blue";
+        }
+
+    }
